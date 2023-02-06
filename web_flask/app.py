737480f7d9d69e64d6947b831dcb8a46cd308c32
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-02-06 10:57:02 +0000

    committing

---
+++ web_flask/app.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
