18f2ae190ae6b6fc6c577ba37ea5bb636a6e8610
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-02-07 09:14:46 +0000

    tough times never last

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+The application listens on 0.0.0.0, port 5005.
 Routes:
     /: Displays 'Hello HBNB!'
 """
@@ -16,4 +16,4 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0' , port=5005)
