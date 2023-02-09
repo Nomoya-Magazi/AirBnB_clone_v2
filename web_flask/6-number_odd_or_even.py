23702b189ab7f4c1fb56f3d46a928c84456906a9
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-02-09 10:43:46 +0000

    redo

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -61,22 +61,22 @@ def intt(n):
         return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/(any integer)", strict_slashes=False)
 def template(n):
-    """/number_template/<int:n>
+    """/number_template/(any integer)
     Args:
-        n (int): <int:n>
+        n (int): (any integer)
     Returns:
         html: render html template
     """
     return render_template("5-number.html", num=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route("/airbnb-dynamic/number_odd_or_even/(any integer)", strict_slashes=False)
 def odd_or_even(n):
-    """/number_odd_or_even/<int:n>
+    """/airbnb-dynamic/number_odd_or_even/(any integer)
     Args:
-        n (int): <int:n>
+        n (int): (any integer)
     Returns:
         template: html template 6-number_odd_or_even.html
     """
