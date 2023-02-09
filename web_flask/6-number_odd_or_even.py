378f45f33665dbb7846c922cc412d5c440a2633b
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2023-02-09 13:02:09 +0200

    redo

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -61,32 +61,20 @@ def intt(n):
         return "{} is a number".format(n)
 
 
-@app.route("/number_template/(any integer)", strict_slashes=False)
-def template(n):
-    """/number_template/(any integer)
-    Args:
-        n (int): (any integer)
-    Returns:
-        html: render html template
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer.
+    Displays the value of <n> in the body.
     """
-    return render_template("5-number.html", num=n)
+    return render_template("5-number.html", n=n)
 
 
-@app.route("/airbnb-dynamic/number_odd_or_even/(any integer)", strict_slashes=False)
-def odd_or_even(n):
-    """/airbnb-dynamic/number_odd_or_even/(any integer)
-    Args:
-        n (int): (any integer)
-    Returns:
-        template: html template 6-number_odd_or_even.html
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Displays an HTML page only if <n> is an integer.
+    States whether <n> is odd or even in the body.
     """
-    if n % 2 == 0:
-        return render_template("6-number_odd_or_even.html",
-                               num="{} is even".format(n))
-    else:
-        return render_template("6-number_odd_or_even.html",
-                               num="{} is odd".format(n))
-
+    return render_template("6-number_odd_or_even.html", n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001', debug=False)
